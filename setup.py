23eb918f7f1441804d91dbd85311d714defdbb26
paulocheque <paulocheque@gmail.com>
2018-09-07 23:34:19 -0300

    New python versions: 3.6 and 3.7

--- setup.py
+++ setup.py
@@ -38,6 +38,8 @@ setup(name='epub_meta',
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: Implementation :: PyPy',
       ],
 
